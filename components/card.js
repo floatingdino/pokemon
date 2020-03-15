00dcf8ca0f289e54eac56014fefee5ff8241375a
Sam <sam@samhaakman.com>
2020-03-15 17:46:11 +1000

    toggleable cards

--- components/card.js
+++ components/card.js
@@ -1,16 +1,49 @@
 import "./card.scss";
 
-export default function Card({ id, name, types }) {
+// TODO: Image
+
+export default function Card({ id, name, types, active, inParty, onClick }) {
   return (
     <article className="cell large-4 text-center card-holder">
-      <div className="card pkmn-card">
-        <img className="card-img" src="" alt={name} width="150" height="150" />
+      {onClick && (
+        <button type="button" onClick={onClick} className="card-tap-area">
+          <span className="show-for-sr">
+            {(active && "Remove from Party") || "Add to Party"}
+          </span>
+        </button>
+      )}
+      <div
+        className={`card pkmn-card h-100 grid-y ${(active && "card-active") ||
+          null}`}>
+        <div className="cell shrink">
+          <img
+            className="card-img"
+            src=""
+            alt={name}
+            width="150"
+            height="150"
+          />
+        </div>
         <div className="card-img-shadow" />
-        <div>{id}</div>
-        <h2>{name}</h2>
-        <ul className="pkmn-types">
-          {types.map(type => <li key={type}>{type}</li>)}
-        </ul>
+        <div className="cell shrink">
+          <div className="card-id" title={`Pokemon ID Number: ${id}`}>
+            {id}
+          </div>
+        </div>
+        <div className="cell grow">
+          <h2>{name}</h2>
+        </div>
+        <div className="cell shrink">
+          <ul className="card-types mt-0 mb-0">
+            {types.map(type => (
+              <li
+                className={`card-type card-type-${type.toLowerCase()}`}
+                key={type}>
+                {type}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </article>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { Component } from "react";
 import Link from "next/link";
+
+import "./index.scss";
 
 import Layout from "../components/layout";
 import Card from "../components/card";
 
+// TODO: load from API (paged)
 const pokemon = [
   {
     id: "001",
@@ -36,30 +40,67 @@ const pokemon = [
   }
 ];
 
-export default function Index() {
-  return (
-    <Layout>
-      <div className="grid-container">
-        <div className="grid-x grid-margin-x">
-          <div className="cell large-1" />
-          <div className="cell large-2">
-            <h1>Choose your team</h1>
+const max_pokemon = 151;
 
-            <button type="button" className="scroll-more">
-              Scroll for more
-            </button>
-          </div>
-          <div className="cell large-6">
-            <div className="grid-x grid-margin-x grid-margin-y">
-              {pokemon.map(mon => <Card {...mon} />)}
+export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: []
+    };
+  }
+
+  // TODO: sync to localStorage or API
+  toggleCardActive(id) {
+    const idIndex = this.state.active.indexOf(id);
+    if (idIndex >= 0) {
+      const newActive = this.state.active;
+      newActive.pop(idIndex);
+      this.setState({
+        active: newActive
+      });
+    } else {
+      this.setState({
+        active: [...this.state.active, id]
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="grid-container">
+          <div className="grid-x grid-margin-x">
+            <div className="cell large-1" />
+            <div className="cell large-2">
+              <h1>Choose your team</h1>
+
+              <button type="button" className="scroll-more">
+                Scroll for more
+              </button>
+            </div>
+            <div className="cell large-6">
+              <div className="grid-x grid-margin-x grid-margin-y align-stretch">
+                {pokemon.map(mon => (
+                  <Card
+                    key={mon.id}
+                    {...mon}
+                    active={this.state.active.indexOf(mon.id) >= 0}
+                    onClick={() => this.toggleCardActive(mon.id)}
+                  />
+                ))}
+                <div className="cell pagination-indicator text-center">
+                  {pokemon.length}/{max_pokemon}
+                </div>
+              </div>
+            </div>
+            <div className="cell large-1" />
+            <div className="cell large-1">
+              <Link href="/party">Party</Link>
             </div>
           </div>
-          <div className="cell large-1" />
-          <div className="cell large-1">
-            <Link href="/party">Party</Link>
-          </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
