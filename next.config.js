9d9b3f8b858f955f441a5f17cb20be68515310f8
Sam <sam@samhaakman.com>
2020-03-18 23:01:48 +1000

    updated next config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,29 @@ module.exports = withOffline(
     },
     env: {
       root: process.env.siteRoot || "http://localhost:3000/"
+    },
+    target: "serverless",
+    transformManifest: manifest => ["/"].concat(manifest),
+    generateInDevMode: true,
+    workboxOpts: {
+      swDest: "static/service-worker.js",
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "https-calls",
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     }
   })
 );
